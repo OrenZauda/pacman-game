b4146cc689bc9e8049ba9d3bb0dab60f97db083b
OrenZauda <45006271+OrenZauda@users.noreply.github.com>
2019-01-07 18:24:59 +0200

    Update myFrame.java

--- myFrame.java
+++ myFrame.java
@@ -13,7 +13,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Set;
-
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JCheckBoxMenuItem;
@@ -25,10 +24,8 @@ import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JRadioButtonMenuItem;
 import javax.swing.filechooser.FileNameExtensionFilter;
-
 import Coords.MyCoords;
 import Geom.Point3D;
-
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -36,9 +33,8 @@ import javax.swing.*;
 
 
 public class myFrame extends JFrame /*implements MouseListener*/ {
+	public Map object= new Map();
 	public boolean isRunning=true;
-	public static final int width= 1433;
-	public static final int height= 642;
 	public static final String title= "Pac-man";
 	JMenuBar menuBar=new JMenuBar();
 	JMenu Run=new JMenu("Run");
@@ -66,19 +62,6 @@ public class myFrame extends JFrame /*implements MouseListener*/ {
 
 	public myFrame() { 
 
-		try
-		{
-			map = new ImageIcon("C:\\temp\\ariel1.png");
-			apple=ImageIO.read(new File("C:\\temp\\apple.png"));
-			pacman = ImageIO.read(new File("C:\\temp\\iconfinder_Cute Ball - Games_32132.png"));
-
-		}
-		catch (IOException e)
-		{ 
-			String workingDir = System.getProperty("user.dir");
-			System.out.println("Current working directory : " + workingDir);
-			e.printStackTrace();
-		}	
 		Run.add(pac);
 		Run.add(fruit);
 		Run.add(save);
@@ -91,7 +74,7 @@ public class myFrame extends JFrame /*implements MouseListener*/ {
 		setJMenuBar(menuBar);
 		setSize(1500, 1500);
 		setTitle("Pacman Game!!!");
-		setIconImage(pacman);
+		setIconImage(object.pacman.getImage());
 		setLayout(new FlowLayout());
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
@@ -153,8 +136,9 @@ public class myFrame extends JFrame /*implements MouseListener*/ {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				while(game.fruits.size()>0){
 				if(game.fruits.size()>0&&game.pacmans.size()>0) {
-					
+
 					ShortestPathAlgo object= new ShortestPathAlgo(game);
 					fruit eaten=new fruit(new Point3D(1,1,1),1,1);
 					fruit foo=new fruit(new Point3D(1,1,1),1,1);
@@ -169,27 +153,27 @@ public class myFrame extends JFrame /*implements MouseListener*/ {
 							eaten=foo;
 						}
 					}
-					
+
 					Iterator <pacman> here= game.pacmans.iterator();
 					while(here.hasNext()) {
 						temp= here.next();
 						if(temp.id==set[0]) {
 							temp2=temp;
 						}
-				
+
 					}
 					System.out.println(temp.toString());
 					System.out.println(eaten.toString());
-					
-					Point3D[] line = drawline(temp2.location,eaten.location);
-					for(int i=0;i<14;i++) {
-					pacman ln=new pacman (line[i],1,1,700);
-					game.pacmans.add(ln);
-					repaint();
-					}
-					for (int i=0;i<game.pacmans.size();i++) {
-						System.out.println(game.pacmans.get(i));
-					}
+
+					//					Point3D[] line = drawline(temp2.location,eaten.location);
+					//					for(int i=0;i<14;i++) {
+					//					pacman ln=new pacman (line[i],1,1,700);
+					//					game.pacmans.add(ln);
+					//					repaint();
+					//					}
+					//					for (int i=0;i<game.pacmans.size();i++) {
+					//						System.out.println(game.pacmans.get(i));
+					//					}
 					game.fruits.remove(eaten);
 					temp2.location.setLon(eaten.location.gety());
 					temp2.location.setAlt(eaten.location.getx());
@@ -202,81 +186,81 @@ public class myFrame extends JFrame /*implements MouseListener*/ {
 
 			}
 
-			private Point3D[] drawline(Point3D a, Point3D b)
-			{
-				MyCoords ob= new MyCoords();
-				
-					Point3D dis= new Point3D(1,1,1);
-					dis.setAlt(b.getx()-a.getx());
-					dis.setLon(b.gety()-a.gety());
-					dis.setAlt(dis.getx()/15);
-					dis.setLon(dis.gety()/15);
-					for (int i = 0; i < result.length; i++) {
-Point3D t=new Point3D(a.getx()+(dis.getx()*(i+1)),a.gety()+dis.gety()*(i+1),0);
-						result[i]=t;
-					
-				}
-				return result;
+			//			private Point3D[] drawline(Point3D a, Point3D b)
+			//			{
+			//				MyCoords ob= new MyCoords();
+			//				
+			//					Point3D dis= new Point3D(1,1,1);
+			//					dis.setAlt(b.getx()-a.getx());
+			//					dis.setLon(b.gety()-a.gety());
+			//					dis.setAlt(dis.getx()/15);
+			//					dis.setLon(dis.gety()/15);
+			//					for (int i = 0; i < result.length; i++) {
+			//Point3D t=new Point3D(a.getx()+(dis.getx()*(i+1)),a.gety()+dis.gety()*(i+1),0);
+			//						result[i]=t;
+			//					
+			//				}
+			//				return result;
+			//			}
 			}
-
 		});
 
-						pac.addActionListener(new ActionListener() {
+		pac.addActionListener(new ActionListener() {
 
-							@Override
-							public void actionPerformed(ActionEvent e) {
-								// TODO Auto-generated method stub
-								f=false;
-								p=true;
-								c=false;
-								addMouseListener(new MouseAdapter() {
-									public void mouseClicked(MouseEvent event) {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				f=false;
+				p=true;
+				c=false;
+				addMouseListener(new MouseAdapter() {
+					public void mouseClicked(MouseEvent event) {
 
-										//TODO Auto-generated method stub
-										if(p&&!f) {
-											x=event.getX();
-											y=event.getY();
-											System.out.println("pacman " +x+" and y "+y);
-											Map object = new Map();		
-											Point3D s=object.pixels2gps(x, y);
-											pacman temp=new pacman(s,1,1,pacid++); 
-											game.pacmans.add(temp);
-											repaint();
+						//TODO Auto-generated method stub
+						if(p&&!f) {
+							y=object.width-event.getX();
+							x=event.getY();
+							System.out.println("pacman " +x+" and y "+y);
+							Map object = new Map();		
+							Point3D s=object.pixels2gps(x, y);
+							pacman temp=new pacman(s,1,1,pacid++); 
+							game.pacmans.add(temp);
+							repaint();
 
-										}
+						}
 
-									}
-								});
+					}
+				});
 
 
 
-							}
-						});
-						fruit.addActionListener(new ActionListener() {
-							@Override
-							public void actionPerformed(ActionEvent e) {
-								// TODO Auto-generated method stub
-								f=true;
-								p=false;
-								c=false;
-								addMouseListener(new MouseAdapter() {
-									public void mouseClicked(MouseEvent event) {					
-										// TODO Auto-generated method stub
-										if(f&&!p) {
-											x=event.getX();
-											y=event.getY();
-											
-											System.out.println("fru "+x+" "+y);
-											Map object = new Map();		
-											Point3D s=object.pixels2gps(x, y);
-											fruit temp=new fruit(s,1,fruitid++);                     	
-											game.fruits.add(temp);
-											repaint();						
-										}
-									}
-								});
-							}
-						});
+			}
+		});
+		fruit.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				f=true;
+				p=false;
+				c=false;
+				addMouseListener(new MouseAdapter() {
+					public void mouseClicked(MouseEvent event) {					
+						// TODO Auto-generated method stub
+						if(f&&!p) {
+							y=object.width-event.getX();
+							x=event.getY();
+
+							System.out.println("fru "+x+" "+y);
+							Map object = new Map();		
+							Point3D s=object.pixels2gps(x, y);
+							fruit temp=new fruit(s,1,fruitid++);                     	
+							game.fruits.add(temp);
+							repaint();						
+						}
+					}
+				});
+			}
+		});
 	}
 
 	public void paint(Graphics g) {
@@ -285,48 +269,79 @@ Point3D t=new Point3D(a.getx()+(dis.getx()*(i+1)),a.gety()+dis.gety()*(i+1),0);
 		Graphics2D g2=(Graphics2D)g;
 
 		// draw the image
-		g2.drawImage(map.getImage(),0,0, getWidth(), getHeight(),null);
+		g2.drawImage(object.icon.getImage(),0,0, getWidth(), getHeight(),null);
 		Map object = new Map();	
 		if(game.fruits!=null&&game.pacmans!=null) {
 			//draw all pacmans in the array
 			Iterator <pacman> here =game.pacmans.iterator();
 			//draw all fruits in the array
-			Iterator <fruit> there =game.fruits.iterator();	
+			Iterator <fruit> there =game.fruits.iterator();
+			//draw pacmans
 			for(int i=0;i<game.pacmans.size();i++){
 
 				pacman temp =here.next();
-				
+
 				Point3D r=new Point3D(temp.location.getx(),temp.location.gety(),0);
 				double g1,s,x=object.gps2pixels(r)[0],y=object.gps2pixels(r)[1];
-				x=(double)x/width;
-				y=(double)y/height;
+				x=(double)x/object.width;
+				y=(double)y/object.height;
 				g1=(double)x*getWidth();
 				s=(double)y*getHeight();
 				if(x>0.25&&x<0.5) {g1=g1+13;}
 				if(x>0.5) {g1=g1+20;}
 				if(x>0.6) {g1=g1+5;}
 				if(x>0.75) {g1=g1+7;}
-				g2.drawImage(pacman,(int)g1,(int)s,15,15,null);
-//				if(temp.id==700) {
-//					game.pacmans.remove(i);
-//				}
+				g2.drawImage(game.pacmans.get(i).icon.getImage(),(int)g1,(int)s,15,15,null);
 
 			}
+			//draw fruits
 			for(int i=0;i<game.fruits.size();i++) {
 				fruit temp2 =there.next();
 				Point3D r=new Point3D(temp2.location.getx(),temp2.location.gety(),0);
-				double g1,s,x=object.gps2pixels(r)[0],y=object.gps2pixels(r)[1];
-				x=(double)x/width;
-				y=(double)y/height;
-				g1=(double)x*getWidth();
-				s=(double)y*getHeight();
-				if(x>0.3) {g1=g1+10;}
-				if(x>0.5) {g1=g1+15;}
-				if(x>0.6) {g1=g1+7;}
-				if(x>0.75) {g1=g1+7;}
-				g2.drawImage(apple,(int)g1,(int)s,15,15,null);
+				x=object.gps2pixels(r)[0];
+				y=object.gps2pixels(r)[1];
+
+				double u=(double)x/object.width;
+				double v=(double)y/object.height;
+
+				double e,d;
+				e=(double)u*getWidth();
+				d=(double)v*getHeight();
+				if(u>0.25) {e=e+10;}
+				if(u>0.5) {e=e+20;}
+				if(u>0.75) {e=e+7;}
+				g2.drawImage(game.fruits.get(i).icon.getImage(),(int)e,(int)d,15,15,null);
+				System.out.println("x= "+x+" . y= "+y);
 			}
-			
+			//draw boxes
+			for(int i=0;i<game.boxes.size();i++) {
+				Point3D a=game.boxes.get(i).location1;
+				Point3D b=game.boxes.get(i).location2;
+				double x1,y1,x2,y2;
+				Map ob= new Map();
+				x1=ob.gps2pixels(a)[0];
+				x1=(double)(x1/object.width*getWidth());
+				y1=ob.gps2pixels(a)[1];
+				y1=(double)(y1/object.height*getHeight());
+				x2=ob.gps2pixels(b)[0];
+				x2=(double)(x2/object.width*getWidth());
+				y2=ob.gps2pixels(b)[1];
+				y2=(double)(y2/object.height*getHeight());
+
+				g2.drawImage(game.boxes.get(i).icon.getImage(),(int)Math.min(x1,x2),(int)Math.min(y1,y2),(int)Math.abs(x1-x2),(int)Math.abs(y1-y2),null);
+			}	
+			//draw ghosts
+			for(int i=0;i<game.ghosts.size();i++) {
+			Point3D a=game.ghosts.get(i).location;
+			Map ob= new Map();
+			double x1,y1;
+            x1=ob.gps2pixels(a)[0];
+            y1=ob.gps2pixels(a)[1];
+            x1=(double)(x1/object.width*getWidth());
+            y1=(double)(y1/object.height*getWidth());
+			g2.drawImage(game.ghosts.get(i).icon.getImage(),x,y,25,25,null);
+
+			}
 		}
 	}
 
@@ -373,11 +388,10 @@ Point3D t=new Point3D(a.getx()+(dis.getx()*(i+1)),a.gety()+dis.gety()*(i+1),0);
 
 	public static void main(String[] args) {
 		myFrame a= new myFrame();
-		a.setSize(a.width,a.height);
+		a.setSize(a.object.width,a.object.height);
 
 	}
 
 
 
 }
-
