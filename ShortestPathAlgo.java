3abec912b46507248e34b982e65d99d84e822f4f
OrenZauda <45006271+OrenZauda@users.noreply.github.com>
2019-01-13 18:02:21 +0200

    Update ShortestPathAlgo.java

--- ShortestPathAlgo.java
+++ ShortestPathAlgo.java
@@ -17,48 +17,61 @@ public class ShortestPathAlgo {
 	public ShortestPathAlgo(game x) {
 		game=x;
 		int i=0;
-		}
-	
+	}
+
 
 	public double[] cal() {
 		//get the time matrix
-		double [][] time = timepath(game.pacmans,game.fruits);
+		double [][] time = timepath(game.myplayer,game.fruits,game.pacmans);
 		//variables to save the shortest time between pacman and fruit
-		int u=0,v=0;
+		int v=0;
 		//find the shortest time
-		double min=time[v][u];
+		double min=time[0][0];
 		for (int i = 0; i < time.length; i++) {
 			for (int j = 0; j < time[0].length; j++) {
 				if(time[i][j]<min) {
 					min=time[i][j];
-					u=i; v=j;
+					 v=j;
 				}
 			}
 		}
-		// change location of the pacman to the location of the fruit
-		game.pacmans.get(u).location.setAlt(game.fruits.get(v).location.getx());
 
-		double [] result= {game.pacmans.get(u).id,game.fruits.get(v).id,time[u][v]};
-		return result;
+		//in case the current chased object is fruit
+		if(v<game.fruits.size()) {
+			double [] result= {0,game.fruits.get(v).id,0};
+			return result;
+		}
+		//in case the current chased object is pacman
+		else {
+			double [] result= {0,game.pacmans.get(v-game.fruits.size()).id,1};
+			return result;
+
+		}
 
 	}
 
-	
 
 
 
-	public double[][] timepath(ArrayList <pacman>a,ArrayList <fruit>b) {
-//		System.out.println(Arrays.toString(b));
-//		System.out.println(Arrays.toString(a));
+
+	public double[][] timepath(pacman a,ArrayList <fruit>b,ArrayList <pacman>c) {
+		//		System.out.println(Arrays.toString(b));
+		//		System.out.println(Arrays.toString(a));
 		MyCoords object=new MyCoords();
-		double [][] temp = new double [a.size()][b.size()];
-		for (int i = 0; i < temp.length; i++) {
-			for (int j =0; j < temp[0].length; j++) {
-				temp[i][j]=object.distance3d(a.get(i).location,b.get(j).location)
-						/a.get(i).mps-a.get(i).Eatradius;
+		double [][] temp = new double [1][b.size()+c.size()];
+
+		for (int j =0; j < temp[0].length; j++) {
+			if(j<b.size()) {
+				temp[0][j]=(object.distance3d(a.location,b.get(j).location)-a.Eatradius)
+						/a.mps;
+			}
+			else {
+				temp[0][j]=(object.distance3d(a.location,c.get(j-b.size()).location)-a.Eatradius)
+						/a.mps;
 			}
 		}
-//        System.out.println(temp.length+" and "+temp[0].length);
+
+		//        System.out.println(temp.length+" and "+temp[0].length);
 		return temp;
 	}
 }
